c7928c6a48521992646b7242e66c561b8c22f9a1
Felix Henneke <felix.henneke@protonmail.com>
2024-12-04 10:37:03 +0100

    some comments

--- dbt_poc/models/staging/dune_data/stg_dune_data__vouch.sql
+++ dbt_poc/models/staging/dune_data/stg_dune_data__vouch.sql
@@ -2,15 +2,15 @@ with
 
 source as (
     select 
-        contract_address,
-        evt_tx_hash as tx_hash,
-        evt_index as index,
-        evt_block_time as block_time,
-        evt_block_number as block_number,
-        "bondingPool" as bonding_pool,
-        "cowRewardTarget" as reward_target,
-        sender,
-        solver
+        contract_address, -- my preferred types: bytea
+        evt_tx_hash as tx_hash, -- bytea
+        evt_index as index, -- bitint
+        evt_block_time as block_time, -- not sure, maybe timestamp
+        evt_block_number as block_number, -- bigint (as is)
+        "bondingPool" as bonding_pool, -- bytea
+        "cowRewardTarget" as reward_target, -- byeta
+        sender, -- bytea
+        solver -- bytea
     from
         {{ source('dune_data', 'VouchRegister_evt_Vouch')}}
 )
