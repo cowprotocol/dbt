b1bad815ebbe24fbcb87716e7ecbbac37fa5516a
Aurelie Ferron <60011303+ferrau10@users.noreply.github.com>
2025-01-30 17:49:18 +0100

    final transfer per solver calculation

--- dbt_poc/models/marts/fct_accounting_period_solver_transfer.sql
+++ dbt_poc/models/marts/fct_accounting_period_solver_transfer.sql
@@ -9,4 +9,35 @@ solver_and_accounting_period as (
     select * from {{ref('fct_data_per_solver_and_accounting_period')}}
 
 )
-select * from solver_and_accounting_period
+
+select 
+    solver,
+    solver_name,
+    reward_target,
+    accounting_period,
+    accounting_period_start_time,
+    accounting_period_end_time,
+    overdraft,
+    overdraft_amount,
+    
+    case 
+        when total_cow_reward_including_fee >= 0 and total_native_reward >= 0 
+            then total_native_reward  
+        when overdraft = true then 0   
+        when total_native_reward < 0
+        	then 0
+        when total_cow_reward_including_fee < 0 
+            then total_native_reward + (total_cow_reward_including_fee * conversion_rate_cow_to_native) 
+    end as total_native_transfer,
+
+    case 
+        when total_cow_reward_including_fee >= 0 and total_native_reward >= 0 
+            then total_cow_reward_including_fee  
+        when overdraft = true then 0 
+        when total_cow_reward_including_fee < 0
+             then 0
+        when total_native_reward < 0 
+            then total_cow_reward_including_fee + (total_native_reward / conversion_rate_cow_to_native) 
+    end as total_cow_transfer
+
+from solver_and_accounting_period
